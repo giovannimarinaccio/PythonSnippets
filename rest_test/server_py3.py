84002bc892eb7389ab6f968fb207635ebcdba769
Giovanni Marinaccio <44344192+giovannimarinaccio@users.noreply.github.com>
2020-03-12 16:23:14 +0100

    Update server_py3.py

--- rest_test/server_py3.py
+++ rest_test/server_py3.py
@@ -10,7 +10,7 @@ import logging
 class S(BaseHTTPRequestHandler):
     def _set_response(self):
         self.send_response(200)
-        self.send_header('Content-type', 'text/html')
+        self.send_header('Content-type', 'application/json')
         self.end_headers()
 
     def do_GET(self):
