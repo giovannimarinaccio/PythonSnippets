52c0ae7625455c9e0cf954e3b0d9fea5d9d71a68
Giovanni Marinaccio <44344192+giovannimarinaccio@users.noreply.github.com>
2021-03-21 18:58:47 +0100

    Create PKCEGenerator.py

---
+++ PKCEGenerator.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Very simple PKCE Generator
+Usage::
+    ./PKCEGenerator.py [<port>]
+"""
+import secrets
+
+def get_new_code_verifier() -> str:
+    token = secrets.token_urlsafe(100)
+    return token[:128]
+
+code_verifier = code_challenge = get_new_code_verifier()
+
+print(len(code_verifier))
+print(code_verifier)
